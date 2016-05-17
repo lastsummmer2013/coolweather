da58ff5fc38e2b7386af631269e7af772e13588b
Administrator <Administrator@303-45>
2016-05-17 10:34:21 +0800

    zgys

---
+++ Calculator/src/test/calculatorTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class calculatorTest extends TestCase {
+
+	public calculatorTest(String name) {
+		super(name);
+	}
+
+	protected static void setUpBeforeClass() throws Exception {
+	}
+
+	protected static void tearDownAfterClass() throws Exception {
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
